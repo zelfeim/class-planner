304caa1a553d952ca05041a8f5532518292e0994
zelfeim <zelfeim@mailbox.org>
2025-04-26 13:42:02 +0200

    init project structure

---
+++ src/ClassPlanner.Core/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ClassPlanner.Core;
+
+public class Class1
+{
+}
---
+++ tests/ClassPlanner.Api.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassPlanner.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
