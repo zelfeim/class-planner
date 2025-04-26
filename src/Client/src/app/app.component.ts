cac4ce801321a32de6c5d2041669935e2ffe604d
michalpostek <73778067+michalpostek@users.noreply.github.com>
2025-04-26 14:17:08 +0200

    add prettier (#3)
    Co-authored-by: michalpostek <michal.postek@protonmail.com>

--- src/Client/src/app/app.component.ts
+++ src/Client/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: "app-root",
+    imports: [RouterOutlet],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.scss"
 })
 export class AppComponent {
-  title = 'Client';
+    title = "Client";
 }
