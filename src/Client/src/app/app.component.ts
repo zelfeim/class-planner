1828e447699291c66775da47b73a98474e0108fc
Michał Postek <postek.michal@protonmail.com>
2025-05-04 00:53:30 +0200

    feat(client): create navigation

--- src/Client/src/app/app.component.ts
+++ src/Client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./components/header/header.component";
+import { NavigationComponent } from "./components/navigation/navigation.component";
 
 @Component({
     selector: "cp-root",
-    imports: [RouterOutlet, HeaderComponent],
+    imports: [RouterOutlet, HeaderComponent, NavigationComponent],
     templateUrl: "./app.component.html",
     styleUrl: "./app.component.scss"
 })
