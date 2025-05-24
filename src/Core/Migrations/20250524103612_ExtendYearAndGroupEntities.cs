96dc39db953c1d21abc6743b49114cbfd7fbcdf8
zelfeim <zelfeim@mailbox.org>
2025-05-24 12:36:53 +0200

    add migration

---
+++ src/Core/Migrations/20250524103612_ExtendYearAndGroupEntities.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendYearAndGroupEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Mode",
+                table: "Years",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Years",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Classification",
+                table: "Groups",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Groups",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mode",
+                table: "Years");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Years");
+
+            migrationBuilder.DropColumn(
+                name: "Classification",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Groups");
+        }
+    }
+}
